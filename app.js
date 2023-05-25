2855cdd821fc23f9e4d3cb87558c0f6ab683bc51
Saiganeshsristla <saiganesh3108@gmail.com>
2023-05-25 15:50:53 +0530

    Assignment First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+    className: "heading",
+  },
+  "Saiganesh Learning React"
+);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(heading);
