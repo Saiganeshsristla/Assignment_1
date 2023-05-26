def6269e733d8b0d230665cc164243673fd15876
Saiganeshsristla <saiganesh3108@gmail.com>
2023-05-26 13:54:02 +0530

    App is ignite with parcel

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+/*
+ ** Parcel does
+ * creates a server
+ * HMR -> Hot Module Reloading
+ * File watcher algorithm - c++
+ * Bundling
+ * Minify
+ * cleaning our code with removing console logs
+ * dev and production build
+ * super fast build algorithm
+ * Image optimization
+ * caching while development
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement(
   "h1",
   {
     id: "title",
     className: "heading",
   },
-  "Saiganesh Learning React"
+  "Saiganesh Learning React with Namaste React"
 );
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
