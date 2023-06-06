ffc85e81e29ebc37c99be6f2b53e433c627afeb1
Saiganeshsristla <saiganesh3108@gmail.com>
2023-06-06 22:40:03 +0530

    learn about class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
-const About = function () {
+import Profile from "./ProfileClass";
+
+const About = () => {
   return (
     <div>
       <h1>About us page</h1>
       <h2>
         This app is build using react during the namaste react live course.
       </h2>
+      <Profile name={"Saiganesh Sristla"} />
     </div>
   );
 };
---
+++ src/components/ProfileClass.js
@@ -0,0 +1,5 @@
+const Profile = (props) => {
+  return <h1>Name: {props.name}</h1>;
+};
+
+export default Profile;
